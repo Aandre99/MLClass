de3fbf655131a52dd07f78fd87f70aaa90ffdf11
Aandre99 <as@ic.ufal.br>
2020-12-20 13:24:06 -0300

    update size and sum funcions

--- 03_Validation/test.py
+++ 03_Validation/test.py
@@ -1,10 +1,16 @@
 x = [1,2,3,4,5,6,7,8,9,10]
 
 def size():
-  pass
+  count = 0
+  for i in x:
+    count+=1
+  return count
 
 def sum():
-  pass
+  sum_ = 0
+  for elem in x:
+    sum_ += elem
+  return sum_
 
 def max(x):
   max = 0
